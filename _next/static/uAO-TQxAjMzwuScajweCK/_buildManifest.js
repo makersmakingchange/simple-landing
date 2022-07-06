9ec50aff94e5b8554ffb189c827fe1424c897569
dansneil <87339062+dansneil@users.noreply.github.com>
2022-07-06 21:45:47 +0000

    Deploying to gh-pages from @ makersmakingchange/simple-landing@964b7419c61262cde1d0f67a1b80673d929ea4b0 🚀

---
+++ _next/static/uAO-TQxAjMzwuScajweCK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/890-c03a096e13b64b7a.js","static/chunks/pages/index-2535402d7bb4d909.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
